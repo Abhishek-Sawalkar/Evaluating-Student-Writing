060961c6f6749285d6f7a7c2f3b2716e46700d70
Abhishek <abhikuroko@gmail.com>
2021-12-31 01:57:10 +0530

    Added config file

---
+++ src/config.py
@@ -0,0 +1,10 @@
+from torch import cuda
+
+config = {'model_name': '../kaggle/input/roberta-base/',
+         'max_length': 512,
+         'train_batch_size':8,
+         'valid_batch_size':16,
+         'epochs':3,
+         'learning_rate':1e-05,
+         'max_grad_norm':10,
+         'device': 'cuda' if cuda.is_available() else 'cpu'}
